538ba283931a49cdbf8c2c0b7bd1d179af53ac65
Tilde Twisttmann <tihe001@stud.kea.dk>
2024-09-03 23:01:23 +0200

    wupp wup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll('section')
+
+sections.forEach( section => {
+    const h2 = section.querySelector('h2')
+    const ps = section.querySelectorAll('p')
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: section,
+            start: "top 50%", // Animationen sættes igang, når toppen a section-elementet rammer 50% ind i viewporten
+            end: "+=200px", // Animationen er slut, når man har scrollet 200px ned
+            scrub: true // Animationen bindes op på scrollbaren
+        }
+    })
+    .from(h2, {
+        opacity: 0,
+    })
+    .from(ps, {
+        opacity: 0,
+        y: 100,
+        stagger: 0.5
+    })
+})
+
